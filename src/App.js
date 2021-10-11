1f627f9fb327645c4742f1a5ee52ec86ca56ce14
Nishu Tyagi <tyaginishu7@gmail.com>
2021-10-11 20:58:07 +0530

    [Nishu Tyagi]Refactored:Added password field in Fundoo App SignIn Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-// import SignUp from '../src/Pages/Registration/SignUp';
+import SignUp from '../src/Pages/Registration/SignUp';
 import SignIn from '../src/Pages/Login/SignIn';
 
 function App() {
   return (
     <div className="App">
-      {/* <SignUp/> */}
+      <SignUp/>
       <SignIn/>
     </div>
   );
