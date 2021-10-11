fe5b4bf1f795fd986a9f0a7f15d89b79db180a8f
Nishu Tyagi <tyaginishu7@gmail.com>
2021-10-11 14:28:30 +0530

    [Nishu Tyagi]Refactored:Added Media Queries for different Devices for Fundoo SignUp Page

--- src/Registration/SignUp.jsx
+++ src/Registration/SignUp.jsx
@@ -12,50 +12,50 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 const SignUp = () => {
     return (
         <div className='signup_main'>
-            <div className='Fundoo_logo'>
-                <p className='first'>F</p>
-                <p className='second'>u</p>
-                <p className='third'>n</p>
-                <p className='first'>d</p>
-                <p className='fifth'>o</p>
-                <p className='second'>o</p>
+            <div className='left_div'>
+                <div className='Fundoo_logo'>
+                    <p className='first'>F</p>
+                    <p className='second'>u</p>
+                    <p className='third'>n</p>
+                    <p className='first'>d</p>
+                    <p className='fifth'>o</p>
+                    <p className='second'>o</p>
+                </div>
+                <div className='title'>Create your Fundoo Account</div>
+                <div className='name_textbox'>
+                    <TextField className="name" id="first_name" label="First name" variant="outlined" size='small' />
+                    <TextField className="name" id="last_name" label="Last name" variant="outlined" size='small'/>
+                </div>
+                <div className='user_name'>
+                    <OutlinedInput className="user" id="user" label= "Username"
+                        endAdornment={<InputAdornment position="end">@gmail.com</InputAdornment>} />
+                    {/* <TextField style={{ width: '25vw'}} id="username" label="Username" variant="outlined" size='small'>
+                    </TextField> */}
+                    <div className='helper_text'>You can use letters, numbers & periods</div>
+                </div>
+                <div>
+                    <button className='button1'>Use my current email address instead</button>
+                </div>
+                <div className='password_text'>
+                    <TextField id="password" label="Password" variant="outlined" size='small' />
+                    <TextField id="confirm" label="Confirm" variant="outlined" size='small' />
+                </div>
+                <div className='para'>
+                    Use 8 or more characters with a mix of letters, numbers & symbols
+                </div>
+                <div className='checkbox'>
+                <FormGroup>
+                    <FormControlLabel control={<Checkbox />} label="Show password" />
+                </FormGroup>
+                </div>
+                <div className="options">
+                    <button className="text">Sign in instead</button>
+                    <button className="next">Next</button>
+                </div>
             </div>
-            <div className='title'>Create your Fundoo Account</div>
-            <img src={account} className='Account_logo' alt="acc_logo" />
-            <div className='name_textbox'>
-                <TextField className="name" id="first_name" label="First name" variant="outlined" size='small' />
-                <TextField className="name" id="last_name" label="Last name" variant="outlined" size='small'/>
-            </div>
-            <div className='user_name'>
-                <OutlinedInput className="user" id="user" label= "Username"
-                    endAdornment={<InputAdornment position="end">@gmail.com</InputAdornment>} />
-
-                {/* <TextField style={{ width: '25vw'}} id="username" label="Username" variant="outlined" size='small'>
-                </TextField> */}
-                <div className='helper_text'>You can use letters, numbers & periods</div>
-            </div>
-            <div>
-                <button className='button1'>Use my current email address instead</button>
-            </div>
-            <div className='password_text'>
-                <TextField id="password" label="Password" variant="outlined" size='small' />
-                <TextField id="confirm" label="Confirm" variant="outlined" size='small' />
-            </div>
-            <div className='para'>
-                Use 8 or more characters with a mix of letters, numbers & symbols
-            </div>
-            <div className='checkbox'>
-            <FormGroup>
-                <FormControlLabel control={<Checkbox />} label="Show password" />
-            </FormGroup>
-            </div>
-
-            <div className="options">
-                <button className="text">Sign in instead</button>
-                <button className="next">Next</button>
-            </div>
-            <div className='img_text'>
-                One account. All of Fundoo working for you.
+            <div className="right_div">            
+                <img src={account} className='Account_logo' alt="acc_logo" />
+                <div className='img_text'>One account. All of Fundoo working for you.</div>
             </div>
         </div>
     )
